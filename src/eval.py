b33f73e7671c65f14f38051d73c580d6361db72f
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-04-04 08:28:45 -0400

    add more blocked words

--- src/eval.py
+++ src/eval.py
@@ -33,7 +33,8 @@ async def on_message(message):
 @bot.command(name='eval')
 async def _eval(ctx, *, body):
     """Evaluates python code"""
-    blocked_words = ['.delete()', 'os', 'subprocess', 'history()']
+    blocked_words = ['.delete()', 'os', 'subprocess', 'history()', '("token")', "('token')",
+                     'aW1wb3J0IG9zCnJldHVybiBvcy5lbnZpcm9uLmdldCgndG9rZW4nKQ==', 'aW1wb3J0IG9zCnByaW50KG9zLmVudmlyb24uZ2V0KCd0b2tlbicpKQ==']
     if ctx.author.id != bot.owner_id:
         for x in blocked_words:
             if x in body:
