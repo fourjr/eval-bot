7307c2f98637f701990548b7d80354c5afb9df7c
fourjr <28086837+fourjr@users.noreply.github.com>
2018-05-31 22:06:59 +0800

    works

--- examples/cog.py
+++ examples/cog.py
@@ -2,98 +2,102 @@ import inspect
 import io
 import textwrap
 import traceback
-import aiohttp
 from contextlib import redirect_stdout
 
-@commands.command(name='eval')
-async def _eval(ctx, *, body):
-    """Evaluates python code"""
-    env = {
-        'ctx': ctx,
-        'bot': bot,
-        'channel': ctx.channel,
-        'author': ctx.author,
-        'guild': ctx.guild,
-        'message': ctx.message,
-        'source': inspect.getsource
-    }
+import aiohttp
+from discord.ext import commands
 
-    def cleanup_code(content):
-        """Automatically removes code blocks from the code."""
-        # remove ```py\n```
-        if content.startswith('```') and content.endswith('```'):
-            return '\n'.join(content.split('\n')[1:-1])
+class Eval:
+    def __init__(self, bot):
+        self.bot = bot
 
-        # remove `foo`
-        return content.strip('` \n')
+    @commands.command(name='eval')
+    async def _eval(self, ctx, *, body):
+        """Evaluates python code"""
+        env = {
+            'ctx': ctx,
+            'bot': self.bot,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            'source': inspect.getsource
+        }
 
-    def get_syntax_error(e):
-        if e.text is None:
-            return f'```py\n{e.__class__.__name__}: {e}\n```'
-        return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
+        def cleanup_code(content):
+            """Automatically removes code blocks from the code."""
+            # remove ```py\n```
+            if content.startswith('```') and content.endswith('```'):
+                return '\n'.join(content.split('\n')[1:-1])
 
-    env.update(globals())
+            # remove `foo`
+            return content.strip('` \n')
 
-    body = cleanup_code(body)
-    stdout = io.StringIO()
-    err = out = None
+        env.update(globals())
 
-    to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+        body = cleanup_code(body)
+        stdout = io.StringIO()
+        err = out = None
 
-    def paginate(text: str):
-        '''Simple generator that paginates text.'''
-        last = 0
-        pages = []
-        for curr in range(0, len(text)):
-            if curr % 1980 == 0:
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        def paginate(text: str):
+            '''Simple generator that paginates text.'''
+            last = 0
+            pages = []
+            for curr in range(0, len(text)):
+                if curr % 1980 == 0:
+                    pages.append(text[last:curr])
+                    last = curr
+                    appd_index = curr
+            if appd_index != len(text)-1:
                 pages.append(text[last:curr])
-                last = curr
-                appd_index = curr
-        if appd_index != len(text)-1:
-            pages.append(text[last:curr])
-        return list(filter(lambda a: a != '', pages))
-    
-    try:
-        exec(to_compile, env)
-    except Exception as e:
-        err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
-        return await ctx.message.add_reaction('\u2049')
+            return list(filter(lambda a: a != '', pages))
+        
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            err = await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+            return await ctx.message.add_reaction('\u2049')
 
-    func = env['func']
-    try:
-        with redirect_stdout(stdout):
-            ret = await func()
-    except Exception as e:
-        value = stdout.getvalue()
-        err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
-    else:
-        value = stdout.getvalue()
-        if ret is None:
-            if value:
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            err = await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            if ret is None:
+                if value:
+                    try:
+                        
+                        out = await ctx.send(f'```py\n{value}\n```')
+                    except:
+                        paginated_text = paginate(value)
+                        for page in paginated_text:
+                            if page == paginated_text[-1]:
+                                out = await ctx.send(f'```py\n{page}\n```')
+                                break
+                            await ctx.send(f'```py\n{page}\n```')
+            else:
                 try:
-                    
-                    out = await ctx.send(f'```py\n{value}\n```')
+                    out = await ctx.send(f'```py\n{value}{ret}\n```')
                 except:
-                    paginated_text = paginate(value)
+                    paginated_text = paginate(f"{value}{ret}")
                     for page in paginated_text:
                         if page == paginated_text[-1]:
                             out = await ctx.send(f'```py\n{page}\n```')
                             break
                         await ctx.send(f'```py\n{page}\n```')
-        else:
-            try:
-                out = await ctx.send(f'```py\n{value}{ret}\n```')
-            except:
-                paginated_text = paginate(f"{value}{ret}")
-                for page in paginated_text:
-                    if page == paginated_text[-1]:
-                        out = await ctx.send(f'```py\n{page}\n```')
-                        break
-                    await ctx.send(f'```py\n{page}\n```')
 
-    if out:
-        await ctx.message.add_reaction('\u2705')  # tick
-    elif err:
-        await ctx.message.add_reaction('\u2049')  # x
-    else:
-        await ctx.message.add_reaction('\u2705')
+        if out:
+            await ctx.message.add_reaction('\u2705')  # tick
+        elif err:
+            await ctx.message.add_reaction('\u2049')  # x
+        else:
+            await ctx.message.add_reaction('\u2705')
+
+def setup(bot):
+    bot.add_cog(Eval(bot))
